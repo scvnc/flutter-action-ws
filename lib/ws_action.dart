f15c01634a6999c4f1c5b7f256679e016250b9d0
Yuri Titov <yuri@parsesoftware.com>
2020-02-03 10:33:48 -0600

    add BadLoginException

--- lib/ws_action.dart
+++ lib/ws_action.dart
@@ -7,6 +7,11 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:async';
 
+/// error thrown during requestUrlencoded process
+class BadLoginException implements Exception {
+  String cause;
+  BadLoginException(this.cause);
+}
 /// TODO: need to return a matched cookie, not hard coded to the very first one
 Future<String> requestUrlencoded(String url, String username, String password) async {
   HttpClient httpClient = new HttpClient();
@@ -19,11 +24,18 @@ Future<String> requestUrlencoded(String url, String username, String password) a
   request.headers.set('Content-Length', bodyBytes.length.toString());
   request.add(bodyBytes);
   HttpClientResponse response = await request.close();
-  //print("response ${response.cookies[0]}");
-  //print("response code ${response.statusCode}");
-  // todo - you should check the response.statusCode
-  //String reply = await response.transform(utf8.decoder).join();
-  httpClient.close();
-  //print("response body $reply");
-  return "${response.cookies[0]}".split(";")[0];
+  if (response.statusCode == 500) {
+    throw new BadLoginException("Server responded with statusCode of 500");
+  }
+  try {
+    //print("response ${response.cookies[0]}");
+    print("response code ${response.statusCode}");
+    // todo - you should check the response.statusCode
+    String reply = await response.transform(utf8.decoder).join();
+    httpClient.close();
+    print("response body $reply");
+    return "${response.cookies[0]}".split(";")[0];
+  } catch (e) {
+    throw new BadLoginException("There was an error parsing for the cookie: $e");
+  }
 }
